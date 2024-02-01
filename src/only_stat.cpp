e10ff357b177b119e494a893250cd8555216d314
Egor Chernook <egor.chernook@yandex.ru>
2024-02-02 00:07:24 +0600

    fix LOL

---
+++ src/only_stat.cpp
@@ -0,0 +1,25 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "stat.hpp"
+
+int main()
+{
+    std::ifstream info{"info.txt"};
+    std::vector<std::string> vals;
+    std::string line{};
+    std::getline(info, line);
+    std::istringstream str{line};
+    for (std::string value; std::getline(str, value, '\t');)
+    {
+        vals.push_back(value);
+    }
+
+    stat::stater::makeStat(std::filesystem::path{vals[0]}, vals[1]);
+    stat::stater::calcGMR(std::filesystem::path{vals[0]});
+    stat::stater::calcP(std::filesystem::path{vals[0]});
+}
